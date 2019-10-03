b224c985ba3b9c645b29e519d9985871eedc7131
sajedul559 <54328015+sajedul559@users.noreply.github.com>
2019-10-04 00:11:37 +0600

    Average Code

---
+++ Average.java
@@ -0,0 +1,15 @@
+
+package average;
+
+
+public class Average {
+
+   
+    public static void main(String[] args) {
+       
+              float a = (float) 5.00;
+              float b =(float) 7.1;
+              float average= a+b/2;
+              System.out.println("Average is ="+average);
+                 }
+}
