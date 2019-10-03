cfccb9a8ab4c0e1e85c1a8bb23a3997200ffdf46
sajedul559 <54328015+sajedul559@users.noreply.github.com>
2019-10-04 00:13:20 +0600

    Add files via upload

---
+++ Employe.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author hp
+ */
+public class Employe {
+    public int id;
+    public String name;
+    public double salary;
+    
+    public void Employee(){
+        System.out.println("Student id is:"+id);
+        System.out.println("Student name is:"+name);
+    }
+    public void Biology(){
+        System.out.println("");
+    }
+    public void Getname(){
+        System.out.println("Name="+name);
+    
+    }
+}
---
+++ Test.java
@@ -0,0 +1,17 @@
+
+package student;
+
+
+public class Test {
+    public static void main(String[]args){
+    
+   Employe obj = new Employe();
+   obj.id=163432559;
+   obj.name="sajedul islam";
+   obj.salary=500;
+   obj.Biology();
+   obj.Employee();
+   obj.Getname();
+    
+    }
+}
