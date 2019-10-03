317ff3baf8076568a513f79d907889253f417584
sajedul559 <54328015+sajedul559@users.noreply.github.com>
2019-10-04 00:05:49 +0600

    Add Encapsulation Code

---
+++ encapsulation1/Person.java
@@ -0,0 +1,26 @@
+
+package encapsulation1;
+
+public class Person {
+    private int age;
+    private String name;
+    public void setName(String NewName){
+    this.name= NewName;
+    }
+    
+    public String getName(){
+    
+    return name;
+    }
+    
+    public void setAge( int Newage)
+    {
+    this.age= Newage;
+    
+    }
+    public int getAge(){
+    return age;
+    }
+  
+    
+}
---
+++ encapsulation1/Test.java
@@ -0,0 +1,17 @@
+
+package encapsulation1;
+
+public class Test {
+    public static void main(String[]args)
+    {
+    
+   Person sajid = new Person();
+   sajid.setName("sajedul islam kairul");
+        System.out.println(sajid.getName());
+        sajid.setAge(45);
+        System.out.println(sajid.getAge());
+    
+    }
+    
+   
+}
